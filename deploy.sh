61dd701984951a4653b1770929fab2e183e6c438
everrise-akira-kosuda <akira.kosuda@ever-rise.co.jp>
2021-02-05 11:18:04 +0900

    isucari.conf

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 NGINX_CONF=/etc/nginx/nginx.conf
+ISUCARI_CONF=/etc/nginx/sites-enabled/isucari.conf
 MYSQLD_CNF=/etc/mysql/mysql.conf.d/mysqld.cnf
 
 # 各設定ファイルのバックアップを取る
 sudo cp $NGINX_CONF $NGINX_CONF+`date +"%Y%m%d_%H%M%S"`
+sudo cp $ISUCARI_CONF $ISUCARI_CONF+`date +"%Y%m%d_%H%M%S"`
 sudo cp $MYSQLD_CNF $MYSQLD_CNF+`date +"%Y%m%d_%H%M%S"`
 
 # 設定ファイルをデプロイする
 sudo cp ./nginx.conf $NGINX_CONF
+sudo cp ./isucari.conf $ISUCARI_CONF
 sudo cp ./mysqld.cnf $MYSQLD_CNF
 
 # サービスを再起動する
